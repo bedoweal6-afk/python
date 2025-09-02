6512b55fa48191f9d3e9d928315f7a1b4da65796
bedoweal6-afk <bedoweal6@gmail.com>
2025-09-02 08:38:56 -0700

    Add files via upload

---
+++ abdo.py
@@ -0,0 +1,4 @@
+x=18
+for x in range(1,20):
+    if x%3==0:
+        print(x)
---
+++ bedo.py
@@ -0,0 +1,9 @@
+temp=9
+if temp>30:
+    print("It's a hot day. Stay hydrated!")
+elif temp>20 and 29:
+    print("It's a warm day. Enjoy the weather!")
+elif temp>10 and 19 :
+    print("It's a cool day. Wear a jacket!")
+else:
+    print("It's a cold day. Stay warm!")
